7c6e0f98399eac96db970ad32fe019357693bd8e
han-k31 <hannasgiron@gmail.com>
2025-10-24 14:06:02 -0600

    Create ejercicio5.c

---
+++ ejercicio5.c
@@ -0,0 +1,15 @@
+/* Girón Knepp Hanna Sofía
+clase 3 ejercicio 5*/
+
+#include <stdio.h>
+
+void main()
+{
+  for(int i = 1; i <=100; i++)
+    {
+      if(i % 3 == 0)
+      {
+        printf("Num: %d\n", i);
+      }
+    }
+}
